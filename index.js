ac03e9deb07a7185d3d3f436a44c43e7c294d201
LamLeTomat <lamletomat@ubuntu.ubuntu-domain>
2020-07-30 11:17:06 +0700

    Create views folder use Pug-template engine

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.listen(port, () => console.log(`App listening on port ${port}`))
+app.listen(port, () => console.log(`App listening on port ${port}`));
 
+//Template engine Pug
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+//route
 app.get('/', (req, res) => {
-    res.send('Hello Lam Dzai')
-})
+    res.render('index', {
+        name: "Lam ngon zai"
+    })
+});
 
 app.get('/users', (req, res) => {
-    res.send('User list')
-})
+    res.render('users/index', {
+        users: [
+            {name: 'Lam', id : 1,},
+            {name : 'Quang', id : 2,}
+        ]
+    });
+});
 
